77e94080e0fb9d33c07d6c81d45588424ee1faf7
Khalid AL-Fawaz <107946457+Klo77@users.noreply.github.com>
2023-03-11 22:38:00 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from "react-chat-engine";
+import './App.css';
+
+const App = ()=>{
+    return(
+        <ChatEngine
+        height = "100vh"
+        projectID="0cb0e6ea-b65d-4307-a703-98fde1c33c62"
+        userName = "klo"
+        userSecret= "1212"/>
+    )
+}
+
+export default App;
